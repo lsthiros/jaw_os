dea94e75f7058c99ce89808ab4e72ff2a3ec8ce2
Louie Thiros <3664023+lsthiros@users.noreply.github.com>
2023-08-16 23:01:38 -0700

    Binary now runs

--- jaw_os/src/main.rs
+++ jaw_os/src/main.rs
@@ -2,6 +2,10 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::arch::global_asm;
+use core::ptr;
+
+global_asm!(include_str!("start.s"));
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -9,6 +13,13 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _rust_start() -> ! {
+    const UART0: *mut u8 = 0x0900_0000 as *mut u8;
+    let out_str = b"IPv6 Only Network Stack\n";
+    for byte in out_str {
+        unsafe {
+            ptr::write_volatile(UART0, *byte);
+        }
+    }
     loop {}
 }
