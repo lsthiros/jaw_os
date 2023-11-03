6e071995127275a64ca6e55b31bcb1a02680f47e
Louie Thiros <3664023+lsthiros@users.noreply.github.com>
2023-11-02 23:21:19 -0700

    Added debug functionality to run_uboot, and renamed it to run_qemu

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,11 +1,22 @@
 #! /usr/bin/env sh
 
+# If KERNEL_DEBUG is defined, set QEMU_ARGS to -s -S and set RELEASE_DIR to debug
+if [ -n "$KERNEL_DEBUG" ]; then
+  QEMU_ARGS="-s -S"
+  RELEASE_DIR=debug
+else
+  RELEASE_DIR=release
+  QEMU_ARGS=""
+fi
+
 if [ -z "$KERNEL_DIR" ]; then
-  KERNEL_DIR=./possum_os/target/aarch64-unknown-none-softfloat/release/possum_os
+  KERNEL_DIR=./possum_os/target/aarch64-unknown-none-softfloat/${RELEASE_DIR}/possum_os
 fi
 
 qemu-system-aarch64 \
   -machine virt \
   -cpu cortex-a53 \
   -nographic \
-  -kernel $KERNEL_DIR
+  -machine gic-version=3,secure=off \
+  -kernel $KERNEL_DIR \
+  ${QEMU_ARGS}
